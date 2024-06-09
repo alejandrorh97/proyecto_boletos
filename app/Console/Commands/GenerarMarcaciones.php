35fd8b2d0a727e0542fcc280f825ff53c91d4a3e
Alejandro Rivas <alejandrorrh97@gmail.com>
2024-06-09 10:41:16 -0600

    mas cambios

--- app/Console/Commands/GenerarMarcaciones.php
+++ app/Console/Commands/GenerarMarcaciones.php
@@ -30,7 +30,7 @@ class GenerarMarcaciones extends Command
             $hora = $codigo->tipo === 'entrada' ? '14:00:00' : '00:00:00';
             $fecha = $codigo->created_at->format('Y-m-d') . ' ' . $hora;
 
-            if ($usuario->marcaciones()->where('codigo_id', $codigo->id)->exists()) {
+            if ($usuario->marcaciones()->whereDate('created_at', $fecha)->exists()) {
                 continue;
             }
 
